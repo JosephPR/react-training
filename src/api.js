44e0c25d88507e02dbb61475a431a5fffdfc9710
JosephPR <reilly4181@gmail.com>
2019-07-01 14:12:06 -0600

    updated api

--- src/api.js
+++ src/api.js
@@ -6,8 +6,8 @@ const PlayerAPI = {
     { number: 1, name: "Web Scraping", position: "learn the basics of web scraping" },
     { number: 2, name: "Python", position: "Learn the basics and build an application that will use beautiful soup, beautiful soup scrapes pandas then something about machine learning" },
     { number: 3, name: "React-Native", position: "differenciate yourself from the curriculum apply what you have learned" },
-    { number: 4, name: "Node/Express", position: "Build a backend for a react app, prepare to incorporate this type of backend into your react project" },
-    { number: 5, name: "Next/Gatsby", position: "Understand the difference between the two, build a simple app in each" },
+    { number: 4, name: "Node-Express", position: "Build a backend for a react app, prepare to incorporate this type of backend into your react project" },
+    { number: 5, name: "Next-Gatsby", position: "Understand the difference between the two, build a simple app in each" },
     { number: 6, name: "Project ideas", position: "Not much yet but i will add as they come." }
   ],
   all: function() { return this.players},
--- src/components/Home/home.js
+++ src/components/Home/home.js
@@ -14,7 +14,7 @@ export default class Home extends Component {
     this.fetchTodos()
   }
 
-  componentWillMount(){
+  componentDidUpdate(){
     this.updateTodo()
   }
 
